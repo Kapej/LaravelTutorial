e2265441e12d4563ff3955344ee70c1b0856c17a
Kacper Trafial <kacper.trafial@live.com>
2015-06-04 12:39:52 +0200

    Dodanie nowego widoku 'about' oraz sciezek

--- app/routes.php
+++ app/routes.php
@@ -22,3 +22,7 @@ Route::get('cats',function(){
 Route::get('cats/{id}',function($id){
 	return "Kot #$id";
 });
+
+Route::get('about',function(){
+	return View::make('about')->with('number_of_cats', 9000);
+});
---
+++ app/views/about.php
@@ -0,0 +1,2 @@
+<h1>O Witrynie</h1>
+<p>Na tej witrynie znajdziesz profile <?php echo $number_of_cats; ?> kotów </p>
